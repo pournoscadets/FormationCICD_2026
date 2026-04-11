a286d753f0b662517a8acbe03406b74060cc2b4f
akoua <akouayao2017@gmail.com>
2026-04-11 13:29:29 +0200

    correct the mistake

--- src/main/java/org/example/CalculateImpl.java
+++ src/main/java/org/example/CalculateImpl.java
@@ -3,7 +3,7 @@ package org.example;
 public class CalculateImpl implements CalculateOperator {
     @Override
     public double sum(double num1, double num2) {
-        return num1 + num2 +2;
+        return num1 + num2;
     }
 
     @Override

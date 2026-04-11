8e88387cb7b0879ed1dca6543a8a19f206364566
akoua <akouayao2017@gmail.com>
2026-04-11 10:15:30 +0200

    basic ci

--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -6,7 +6,7 @@ public class CalculateTest {
     @Test
     void testSum() {
         CalculateImpl calc = new CalculateImpl();
-        assertEquals(5.0, calc.sum(2.0, 3.0));
+        assertEquals(5.0, calc.sum(1.0, 3.0));
     }
 }
 

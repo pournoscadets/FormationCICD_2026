952ef6653aa6dadc77e7c1eae3e0c1ec90c6b686
akoua <akouayao2017@gmail.com>
2026-04-11 13:27:13 +0200

    feat: new operation

--- src/main/java/org/example/CalculateImpl.java
+++ src/main/java/org/example/CalculateImpl.java
@@ -3,6 +3,11 @@ package org.example;
 public class CalculateImpl implements CalculateOperator {
     @Override
     public double sum(double num1, double num2) {
-        return num1 + num2;
+        return num1 + num2 +2;
+    }
+
+    @Override
+    public double minus(double num1, double num2) {
+        return num1 - num2;
     }
 }
--- src/main/java/org/example/CalculateOperator.java
+++ src/main/java/org/example/CalculateOperator.java
@@ -3,4 +3,5 @@ package org.example;
 public interface CalculateOperator {
 
     double sum(double num1, double num2);
+    double minus(double num1, double num2);
 }
